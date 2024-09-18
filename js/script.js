f646d56f7f4b7b6f7ebcc730be814d50506e01bf
Izzy Figeuroa <izzyfigueroa6@gmail.com>
2024-09-18 11:55:57 -0400

    finished conditional overview

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 const firstName = 'Izzy'
-let age = 33;
+let age = 18;
 
-// age = age + 1;
-// age += 1; //shorthand
-age++ //shorterhand
-console.log(age);
+// If age is greater than 20 and less than 50, console log 'you are young'
+if (age > 20 && age < 50){
+    console.log('you are young');
+ } else if (age >= 50) {
+    console.log('you are getting older');
+
+} else if (age < 20 && age > 12) {
+    console.log('you are a teenager')
+}else {
+    console.log('your age is not withing our range')
+} 
+ 
+// If age is greater than or equal to 50, console log 'you are getting older'
+
+
+// If age is less than 20 and greater than 12, console log 'you are a teenager'
