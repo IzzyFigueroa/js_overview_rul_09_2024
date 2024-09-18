e9fda1800990f4bde5998caf0d31b7e90d20c86c
Izzy Figeuroa <izzyfigueroa6@gmail.com>
2024-09-18 13:50:35 -0400

    finished for loop basics

--- js/script.js
+++ js/script.js
@@ -1,27 +1,57 @@
 // Arrays are not primitive value types
 // They are called reference types
-const fruits = ['apple', 'orange', 'kiwi', 'grapes'];
+const fruits = [
+    "Apple", "Banana", "Cherry", "Date", "Elderberry",
+    "Fig", "Grape", "Honeydew", "Kiwi", "Lemon",
+    "Mango", "Nectarine", "Orange", "Papaya", "Quince",
+    "Raspberry", "Strawberry", "Tangerine", "Ugli fruit", "Vanilla bean",
+    "Watermelon", "Xigua", "Yellow passion fruit", "Zucchini", "Apricot",
+    "Blackberry", "Cantaloupe", "Dragonfruit", "Elderflower", "Fennel",
+    "Gooseberry", "Huckleberry", "Indian Fig", "Jackfruit", "Kumquat",
+    "Lime", "Mulberry", "Nutmeg fruit", "Olive", "Peach"
+];
 const names = ['Roman', 'Michael', 'Nina', 'Adonis']
 const data = ['Izzy', "Figueroa", 33, '555 coding st', '777-777-7777', ['pickleball', 'fishing']];
 
-console.log(fruits);
+const fullName = 'Izzy Figueroa';
 
-fruits[2] = 'strawberry';
-
-//Incrse the data age number by one - console.log the original value and the new value after you increase it by one.
-
-console.log(data[2]);
-
-data[2]++;
-
-console.log(data[2]);
+for (let index =  0; index < fullName.length; index++) {
+    console.log(fullName[index]);
+}
 
 
+// for (let count =  1; count <= 10; count++) {
+//     console.log(count)
+// }
 
-let fullName = 'Izzy'
 
-console.log(fullName)
 
-fullName += ' Figueroa'
 
-console.log(fullName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let fullName = 'Izzy'
+
+// console.log(fullName)
+
+// fullName += ' Figueroa' //shorthand to add 
+
+// console.log(fullName)
