e6884cd10377ef0dc4877a43bb6a016f4fe5ae8c
Izzy Figeuroa <izzyfigueroa6@gmail.com>
2024-09-18 20:05:20 -0400

    function basics

--- js/script.js
+++ js/script.js
@@ -1,38 +1,12 @@
-// Arrays are not primitive value types
-// They are called reference types
-const fruits = [
-    "Apple", "Banana", "Cherry", "Date", "Elderberry",
-    "Fig", "Grape", "Honeydew", "Kiwi", "Lemon",
-    "Mango", "Nectarine", "Orange", "Papaya", "Quince",
-    "Raspberry", "Strawberry", "Tangerine", "Ugli fruit", "Vanilla bean",
-    "Watermelon", "Xigua", "Yellow passion fruit", "Zucchini", "Apricot",
-    "Blackberry", "Cantaloupe", "Dragonfruit", "Elderflower", "Fennel",
-    "Gooseberry", "Huckleberry", "Indian Fig", "Jackfruit", "Kumquat",
-    "Lime", "Mulberry", "Nutmeg fruit", "Olive", "Peach"
-];
-const names = ['Roman', 'Michael', 'Nina', 'Adonis']
-const data = ['Izzy', "Figueroa", 33, '555 coding st', '777-777-7777', ['pickleball', 'fishing']];
+const names = ['Izzy', 'Matt', 'City', 'Kandyce'];
 
-const fullName = 'Izzy Figueroa';
-
-for (let index =  0; index < fullName.length; index++) {
-    console.log(fullName[index]);
+function printNames () {
+    for (let index = 0; index < names.length; index++) {
+        console.log(names[index]);  
+        }  
 }
 
-
-// for (let count =  1; count <= 10; count++) {
-//     console.log(count)
-// }
-
-
-
-
-
-
-
-
-
-
+printNames();
 
 
 
