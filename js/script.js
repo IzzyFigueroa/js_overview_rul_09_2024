a4430d050585359d2e5d139e6ed4916397016801
Izzy Figeuroa <izzyfigueroa6@gmail.com>
2024-09-20 10:58:43 -0400

    finished object basics

--- js/script.js
+++ js/script.js
@@ -1,23 +1,47 @@
-const notes = ['JS is cool', 'create a constant with const', 'use the src attribute on the script tag to load a js file'];
+const userData = {
+    name: 'JD',
+    age: 44,
+    phone: '777-777-7777',
+    address: '555 coding st',
+    info: {
+      location: 'Atlanta area',
+      hobbies: ['pickleball', 'fishing']
+    },
+    printAge: function () {
+      console.log(this.age);
+    },
+    haveBirthday: function () {
+      // Increase the age of the user by one
+      this.age++;
+      // Print 'Happy Birthday' to the console
+      console.log('Happy Birthday!');
+    },
+    addHobby: function (hobby) {
+      // Need to reference an argument that is passed to addHobby (string of a hobby)
+        this.info.hobbies.push(hobby);
+        // console.log(this.info.hobbies);
+      // Push the argument(hobby) to the info.hobbies array
+     },
+    printHobbies: function () {
+    //   // console.log the hobbies array
+        // 
+        console.log(this.info.hobbies);
+       
+      // BONUS - Loop over the hobbies array and console.log each hobby
+      for (let i = 0; i < this.info.hobbies.length; i++) {
+        console.log(this.info.hobbies[1]);
+      }
+    }
     
-console.log(notes);
+  };
+  console.log(userData.age);
+  userData.addHobby('basketball');
 
-notes.push('copilot is a great help!'); // Adds an item to the end of an array
+  userData.printHobbies();
 
-console.log(notes);
+  // function test(num, word) {
+  //   console.log(num);
+  // }
 
-notes.unshift('CSS is crazy!'); //adds an item to the beginning of an arrow
+  // test(1, 'help');
 
-notes.pop(); //pop removes the last item of an array
-
-console.log(notes); 
-
-notes.shift(); //removes the first item of the array
-
-console.log(notes);
-
-const notes2 = notes;
- // remove an item at a certain index
- //you pass in the index of where you want to start deleting items and then the amount of items you want to move
- notes.splice(1,1);
-    console.log(notes2);
